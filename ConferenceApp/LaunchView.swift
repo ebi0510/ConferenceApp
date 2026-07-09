4ce960d530c0aa88fe6dd8df7dc785a548a4989f
ebi0510 <yasuda.haruna.hk@gmail.com>
2026-07-10 01:15:44 +0900

    タイトル画面作成

---
+++ ConferenceApp/LaunchView.swift
@@ -0,0 +1,22 @@
+//
+//  LaunchView.swift
+//  ConferenceApp
+//
+//  Created by 岡野春菜 on 2026/07/10.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        ZStack {
+            Color("Background")
+                .ignoresSafeArea()
+            Text("TechPR Flow")
+        }
+    }
+}
+
+#Preview {
+    LaunchView()
+}
