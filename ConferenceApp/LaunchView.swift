1503c65e329c3318c602251ab5c698569a341cf4
ebi0510 <yasuda.haruna.hk@gmail.com>
2026-07-12 23:17:50 +0900

    タイトル画面実装

--- ConferenceApp/LaunchView.swift
+++ ConferenceApp/LaunchView.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct LaunchView: View {
+    @State var isActive = false
+    
     var body: some View {
-        ZStack {
-            Color("Background")
-                .ignoresSafeArea()
-            Text("TechPR Flow")
+        if isActive {
+            ContentView()
+        } else {
+            ZStack {
+                Color("Background")
+                    .ignoresSafeArea()
+                VStack(alignment: .center) {
+                    Circle()
+                        .fill(Color("Accent"))
+                        .frame(width: 15)
+                    RoundedRectangle(cornerRadius: 4)
+                        .fill(Color("TextSecondary"))
+                        .frame(width: 6, height: 50)
+                    
+                    Text("TechPR Flow")
+                        .font(.custom("JetBrains Mono", size: 22))
+                        .foregroundStyle(.white)
+                }
+            }
+            .transition(.opacity)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                    withAnimation(.easeInOut(duration: 0.5)){
+                        isActive = true
+                    }
+                }
+            }
         }
     }
 }
